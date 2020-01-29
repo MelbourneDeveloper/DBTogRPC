94952a92af6b49cfd9b727356e27eefea898667f
Christian Findlay <christian@zedtechnologies.com.au>
2020-01-29 19:11:15 +1100

    Can save and load a person on Xamarin

--- src/DBTogRPC.XamarinForms/DBTogRPC.XamarinForms/Views/ItemsPage.xaml.cs
+++ src/DBTogRPC.XamarinForms/DBTogRPC.XamarinForms/Views/ItemsPage.xaml.cs
@@ -10,6 +10,8 @@ using Xamarin.Forms.Xaml;
 using DBTogRPC.XamarinForms.Models;
 using DBTogRPC.XamarinForms.Views;
 using DBTogRPC.XamarinForms.ViewModels;
+using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
 
 namespace DBTogRPC.XamarinForms.Views
 {
@@ -44,12 +46,48 @@ namespace DBTogRPC.XamarinForms.Views
             await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
         }
 
-        protected override void OnAppearing()
+        protected async override void OnAppearing()
         {
             base.OnAppearing();
 
             if (viewModel.Items.Count == 0)
                 viewModel.LoadItemsCommand.Execute(null);
+
+
+            var channel = new Channel("127.0.0.1:15000", ChannelCredentials.Insecure);
+
+            var dbTogRPCServiceClient = new DBTogRPCService.DBTogRPCServiceClient(channel);
+
+            var personKey = new Guid("07b10373-0487-4281-b768-81fdc48c0318");
+
+            var addressKey = new Guid("669c71bf-7c4e-4536-9642-bce10f22b7bd");
+
+            //Save the Person on the server
+            var reply = dbTogRPCServiceClient.Save(
+                new SaveRequest
+                {
+                    TypeName = "Person",
+                    DTO =
+                    Any.Pack
+                    (
+                        new Person
+                        {
+                            PersonKey = personKey.ToString(),
+                            FirstName = "you",
+                            BillingAddress = new Address
+                            {
+                                AddressKey = addressKey.ToString()
+                            }
+
+                        }
+                   )
+                });
+
+            //Load the Person from the server
+            var any = dbTogRPCServiceClient.Get(new DTORequest { TypeName = "Person", KeyValue = personKey.ToString() });
+            var person = any.Unpack<Person>();
+
+            await DisplayAlert("First Name", person.FirstName, "OK");
         }
     }
 }
